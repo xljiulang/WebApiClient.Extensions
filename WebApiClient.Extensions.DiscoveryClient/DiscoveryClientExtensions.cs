301ada2460a247b636fab5f87bb2199d9fcbe71a
xljiulang <366193849@qq.com>
2018-09-23 03:28:23 +0800

    添加项目文件。

---
+++ WebApiClient.Extensions.DiscoveryClient/DiscoveryClientExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Steeltoe.Common.Discovery;
+using System;
+using WebApiClient.Extensions.HttpClientFactory;
+
+namespace WebApiClient.Extensions.DiscoveryClient
+{
+    /// <summary>
+    /// 基于HttpClientFactory的DiscoveryClient扩展
+    /// </summary>
+    public static class DiscoveryClientExtensions
+    {
+        /// <summary>
+        /// 添加Discovery的别名HttpClient
+        /// </summary>
+        /// <typeparam name="TInterface">接口类型</typeparam>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddDiscoveryTypedClient<TInterface>(this IServiceCollection services)
+            where TInterface : class, IHttpApi
+        {
+            return services.AddDiscoveryTypedClient<TInterface>(default(Action<HttpApiConfig, IServiceProvider>));
+        }
+
+        /// <summary>
+        /// 添加Discovery的别名HttpClient
+        /// </summary>
+        /// <typeparam name="TInterface">接口类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="config">http接口的配置</param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddDiscoveryTypedClient<TInterface>(this IServiceCollection services, Action<HttpApiConfig> config)
+            where TInterface : class, IHttpApi
+        {
+            return services.AddDiscoveryTypedClient<TInterface>((c, p) => config?.Invoke(c));
+        }
+
+        /// <summary>
+        /// 添加Discovery的别名HttpClient
+        /// </summary>
+        /// <typeparam name="TInterface">接口类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="config">http接口的配置</param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddDiscoveryTypedClient<TInterface>(this IServiceCollection services, Action<HttpApiConfig, IServiceProvider> config)
+            where TInterface : class, IHttpApi
+        {
+            return services
+                .AddHttpApiTypedClient<TInterface>(config)
+                .ConfigurePrimaryHttpMessageHandler(provider =>
+                {
+                    var discoveryClient = provider.GetService<IDiscoveryClient>();
+                    return new DiscoveryHttpClientHandler(discoveryClient);
+                });
+        }
+    }
+}
---
+++ WebApiClient.Extensions.HttpClientFactory/HttpClientFactoryExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace WebApiClient.Extensions.HttpClientFactory
+{
+    /// <summary>
+    /// 基于HttpClientFactory的扩展
+    /// </summary>
+    public static class HttpClientFactoryExtensions
+    {
+        /// <summary>
+        /// 添加HttpApiClient的别名HttpClient
+        /// </summary>
+        /// <typeparam name="TInterface">接口类型</typeparam>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddHttpApiTypedClient<TInterface>(this IServiceCollection services)
+            where TInterface : class, IHttpApi
+        {
+            return services.AddHttpApiTypedClient<TInterface>(default(Action<HttpApiConfig, IServiceProvider>));
+        }
+
+        /// <summary>
+        /// 添加HttpApiClient的别名HttpClient
+        /// </summary>
+        /// <typeparam name="TInterface">接口类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="config">http接口的配置</param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddHttpApiTypedClient<TInterface>(this IServiceCollection services, Action<HttpApiConfig> config)
+            where TInterface : class, IHttpApi
+        {
+            return services.AddHttpApiTypedClient<TInterface>((c, p) => config?.Invoke(c));
+        }
+
+        /// <summary>
+        /// 添加HttpApiClient的别名HttpClient
+        /// </summary>
+        /// <typeparam name="TInterface">接口类型</typeparam>
+        /// <param name="services"></param>
+        /// <param name="config">http接口的配置</param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddHttpApiTypedClient<TInterface>(this IServiceCollection services, Action<HttpApiConfig, IServiceProvider> config)
+            where TInterface : class, IHttpApi
+        {
+            return services
+                .AddHttpClient<TInterface>()
+                .AddTypedClient((httpClient, provider) =>
+                {
+                    var httpApiConfig = new HttpApiConfig(httpClient);
+                    config?.Invoke(httpApiConfig, provider);
+                    return HttpApiClient.Create<TInterface>(httpApiConfig);
+                });
+        }
+    }
+}
