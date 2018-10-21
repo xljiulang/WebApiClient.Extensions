ff1e01810abed95329fa851680f4dd42ca0dba7e
xljiulang <366193849@qq.com>
2018-10-21 17:16:10 +0800

    增加MessagePack序列化扩展

---
+++ WebApiClient.Extensions.MessagePack/MessagePackContentAttribute.cs
@@ -0,0 +1,53 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示使用MessagePack序列化参数值作为请求内容
+    /// </summary>
+    public class MessagePackContentAttribute : HttpContentAttribute
+    {
+        /// <summary>
+        /// 格式化解析实例
+        /// </summary>
+        private IFormatterResolver formatterResolver = ContractlessStandardResolver.Instance;
+
+        /// <summary>
+        /// 获取或设置请求的内容类型
+        /// 默认为application/x-msgpack
+        /// </summary>
+        public string ContentType { get; set; } = "application/x-msgpack";
+
+        /// <summary>
+        /// 获取或设置IFormatterResolver的类型
+        /// 默认为ContractlessStandardResolver
+        /// </summary>
+        public Type FormatterResolverType
+        {
+            get => this.formatterResolver.GetType();
+            set => this.formatterResolver = Activator.CreateInstance(value) as IFormatterResolver;
+        }
+
+        /// <summary>
+        /// 设置请求Content
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="parameter"></param>
+        protected override void SetHttpContent(ApiActionContext context, ApiParameterDescriptor parameter)
+        {
+            var bytes = MessagePackSerializer.NonGeneric.Serialize(
+                parameter.ParameterType,
+                parameter.Value,
+                this.formatterResolver);
+
+            var content = new ByteArrayContent(bytes);
+            content.Headers.ContentType = new MediaTypeHeaderValue(this.ContentType);
+            context.RequestMessage.Content = content;
+        }
+    }
+}
---
+++ WebApiClient.Extensions.MessagePack/MessagePackReturnAttribute.cs
@@ -0,0 +1,60 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示使用MessagePack反序列化回复内容作为返回值
+    /// </summary>
+    public class MessagePackReturnAttribute : JsonReturnAttribute
+    {
+        /// <summary>
+        /// 格式化解析实例
+        /// </summary>
+        private IFormatterResolver formatterResolver = ContractlessStandardResolver.Instance;
+
+        /// <summary>
+        /// 获取或设置Accept请求头
+        /// 默认为application/x-msgpack
+        /// </summary>
+        public string Accept { get; set; } = "application/x-msgpack";
+
+        /// <summary>
+        /// 获取或设置IFormatterResolver的类型
+        /// 默认为ContractlessStandardResolver
+        /// </summary>
+        public Type FormatterResolverType
+        {
+            get => this.formatterResolver.GetType();
+            set => this.formatterResolver = Activator.CreateInstance(value) as IFormatterResolver;
+        }
+
+        /// <summary>
+        /// 配置Accept请求头
+        /// </summary>
+        /// <param name="accept">Accept请求头</param>
+        protected override void ConfigureAccept(HttpHeaderValueCollection<MediaTypeWithQualityHeaderValue> accept)
+        {
+            accept.TryParseAdd(this.Accept);
+        }
+
+        /// <summary>
+        /// 获取数据结果
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        protected override async Task<object> GetTaskResult(ApiActionContext context)
+        {
+            var bytes = await context.ResponseMessage.Content.ReadAsByteArrayAsync();
+
+            return MessagePackSerializer.NonGeneric.Deserialize(
+                context.ApiActionDescriptor.Return.DataType.Type,
+                bytes,
+                this.formatterResolver);
+        }
+    }
+}
