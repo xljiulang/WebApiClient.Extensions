594d38ed36c143793c421e2edb63d0f55c91dbf7
xljiulang <366193849@qq.com>
2018-10-21 21:52:33 +0800

    增加Protobuf扩展

---
+++ WebApiClient.Extensions.Protobuf/ProtobufContentAttribute.cs
@@ -0,0 +1,39 @@
+﻿using ProtoBuf;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示使用Protobuf序列化参数值作为请求内容
+    /// </summary>
+    public class ProtobufContentAttribute : HttpContentAttribute
+    {
+        /// <summary>
+        /// 获取或设置请求的内容类型
+        /// 默认为application/x-protobuf
+        /// </summary>
+        public string ContentType { get; set; } = "application/x-protobuf";
+
+        /// <summary>
+        /// 设置请求Content
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="parameter"></param>
+        protected override void SetHttpContent(ApiActionContext context, ApiParameterDescriptor parameter)
+        {
+            var stream = new MemoryStream();
+            if (parameter.Value != null)
+            {
+                Serializer.NonGeneric.Serialize(stream, parameter.Value);
+                stream.Position = 0L;
+            }
+
+            var content = new StreamContent(stream);
+            content.Headers.ContentType = new MediaTypeHeaderValue(this.ContentType);
+            context.RequestMessage.Content = content;
+        }
+    }
+}
---
+++ WebApiClient.Extensions.Protobuf/ProtobufReturnAttribute.cs
@@ -0,0 +1,41 @@
+﻿using ProtoBuf;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Attributes
+{
+    /// <summary>
+    /// 表示使用Protobuf反序列化回复内容作为返回值
+    /// </summary>
+    public class ProtobufReturnAttribute : ApiReturnAttribute
+    {
+        /// <summary>
+        /// 获取或设置Accept请求头
+        /// 默认为application/x-protobuf
+        /// </summary>
+        public string Accept { get; set; } = "application/x-protobuf";
+
+        /// <summary>
+        /// 配置Accept请求头
+        /// </summary>
+        /// <param name="accept">Accept请求头</param>
+        protected override void ConfigureAccept(HttpHeaderValueCollection<MediaTypeWithQualityHeaderValue> accept)
+        {
+            accept.TryParseAdd(this.Accept);
+        }
+
+        /// <summary>
+        /// 获取数据结果
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        protected override async Task<object> GetTaskResult(ApiActionContext context)
+        {
+            var stream = await context.ResponseMessage.Content.ReadAsStreamAsync();
+            return Serializer.NonGeneric.Deserialize(
+                context.ApiActionDescriptor.Return.DataType.Type,
+                stream);
+        }
+    }
+}
